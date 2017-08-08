10f4e0c88fc3e27fd2de5bd938c895bfa5cf404b
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-08-08 16:23:36 +0700

    Add jQuery, contenteditable foundation, and basic file save feature

--- editor.js
+++ editor.js
@@ -1,7 +1,19 @@
+// jQuery
+window.$ = window.jQuery = require('jquery');
 // Module to register keyboard shortcuts
 const mousetrap = require('mousetrap');
+// Module to deal with filesystem
+const fs = require('fs');
 
 // Register 'Cmd+S' and 'Ctrl+S' shortcuts listener to save file
-mousetrap.bind(['command+s', 'ctrl+s'], function onBind() {
-  alert('Cmd+S is pressed'); // DEBUG
+mousetrap.bind(['command+s', 'ctrl+s'], function onShortcutPressed() {
+  var text = $('#editor-div').html().trim();
+
+  fs.writeFile('./saved-tes-file.ubud', text, function onSuccess(err) {
+    if(err)
+      return console.log(err); // DEBUG
+
+    alert('File \"saved-tes-file.ubud\" saved.'); // DEBUG
+    fs.close();
+  });
 });
