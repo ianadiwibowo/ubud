d3cac8aa7de4716687f3e0c9c4fe969c9ec5fd33
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2017-08-17 00:10:00 +0700

    Add fountain-js and basic screenplay formatting

--- editor.js
+++ editor.js
@@ -6,6 +6,8 @@ const mousetrap = require('mousetrap');
 const fs = require('fs');
 // Module for interprocess connection between main.js and editor.js
 const {ipcRenderer} = require('electron');
+// Fountain.js parser
+fountain = require('./lib/fountain.js');
 
 // Save file
 ipcRenderer.on('save', (event, arg) => {
@@ -18,4 +20,56 @@ ipcRenderer.on('save', (event, arg) => {
     alert('File \"saved-tes-file.ubud\" saved.'); // DEBUG
     fs.close();
   });
-})
+});
+
+// Editor text type shortcuts 'Cmd/Ctrl+1' to 'Cmd/Ctrl+7'
+var editorTextType = $('#editor-text-type');
+mousetrap.bind(['command+1', 'ctrl+1'], function onCmdOrCtrl1Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=1]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+2', 'ctrl+2'], function onCmdOrCtrl2Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=2]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+3', 'ctrl+3'], function onCmdOrCtrl3Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=3]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+4', 'ctrl+4'], function onCmdOrCtrl4Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=4]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+5', 'ctrl+5'], function onCmdOrCtrl5Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=5]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+6', 'ctrl+6'], function onCmdOrCtrl6Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=6]')
+    .attr('selected', true);
+});
+mousetrap.bind(['command+7', 'ctrl+7'], function onCmdOrCtrl7Pressed() {
+  $('#editor-text-type option')
+    .removeAttr('selected')
+    .filter('[value=7]')
+    .attr('selected', true);
+});
+
+// Load initial file
+fs.readFile('./samples/brick&steel.fountain', function onRead(err, data) {
+  fountain.parse(data.toString(), function(screenplay) {
+    $('#editor-div').html(screenplay.html.script);
+  });
+});
